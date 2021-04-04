6f79235ef07b14aec5726eb1c3e15ddb8017f2c7
Ibrahim Awani-Mayaki <ibrahim.awani-mayaki@luminorgroup.com>
2021-04-04 20:10:17 +0300

    placehoolder for playlist

--- src/components/PlayListView.tsx
+++ src/components/PlayListView.tsx
@@ -16,21 +16,7 @@ const PlayListView = ({playlist, setCurrentSong}: PlayListView) => {
     if (tracks === undefined) return null;
 
     return (<div>
-        <h1 className={"ui header inverted"} style={{marginTop: 10, marginLeft: 10}}>{playlist.name}</h1>
-        <Segment basic>
-            <Card.Group doubling centered>
-                {(tracks.items).map((item: any, index: number) => {
-                    return (
-                        <React.Fragment key={index}>
-                            <MusicCard image={item.track.album.images[0] && item.track.album.images[0].url}
-                                       trackName={item.track.name}
-                                       artist={item.track.artists[0].name}
-                                       onClick={() => setCurrentSong(item.track)} id={item.track.id}/>
-                        </React.Fragment>
-                    )
-                })}
-            </Card.Group>
-        </Segment>
+       <h1>Coming Soon...</h1>
     </div>);
 };
 
